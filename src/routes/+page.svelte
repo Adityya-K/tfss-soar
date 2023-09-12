e55a81ee842a28dd67e466350e6b497c32c47eec
Adityya Kaushal <Adityya-K>
2023-09-12 16:30:32 -0400

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '/src/styles/home.css';
+</script>
+
+<div class="home-main" />
+<div class="home-video">
+	<video playsinline autoplay muted loop>
+		<source src="/src/background.mp4" type="video/mp4" />
+		Your browser doesn't support the video toDateString();
+	</video>
+</div>
+<div class="home-title">
+	<h1>SOAR</h1>
+	<h1>2023</h1>
+</div>
