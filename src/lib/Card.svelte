b874ca34de2ac691793e1c1a8e6d0bce9b09984c
Adityya Kaushal <adityyakaushal@gmail.com>
2023-09-25 09:24:53 -0400

    added content to the page

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -4,10 +4,11 @@
 
 <style>
 	.card {
-		background: #fff;
+		background: #322d31;
+		opacity: 0.85;
 		padding: 20px;
 		border-radius: 6px;
-		box-shadow: 0px 2px 4px rgba(255, 255, 255, 1);
+		box-shadow: 0px 2px 4px rgba(0, 0, 0, 1);
 		height: 250px;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,16 +18,99 @@
 </div>
 <div class="grid-section">
 	<div class="grid">
-		<Card>1</Card>
-		<Card>2</Card>
-		<Card>3</Card>
-		<Card>4</Card>
-		<Card>5</Card>
-		<Card>6</Card>
-		<Card>7</Card>
-		<Card>8</Card>
+		<Card>
+			<h1>Soar Conference</h1>
+			<p>
+				Annual student-led conference for Gr. 7&8 students across peel! This year, SOAR will be
+				taking place on June 3, 2023. Join us as we cultivate our SOARfam® of leaders and create
+				life long memories 💫
+			</p>
+		</Card>
+		<Card>
+			<div class="image">
+				<img
+					src="https://plus.unsplash.com/premium_photo-1673697239984-b089baf7b6e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmFja2dyb3VuZCUyMGltYWdlfGVufDB8fDB8fHww&w=1000&q=80"
+					alt="Background"
+				/>
+			</div>
+		</Card>
+		<Card>
+			<h1>Opportunity Fair</h1>
+			<p>
+				Promote your club or organization to delegates and students at the opportunity fair!
+				<a
+					href="https://docs.google.com/forms/d/e/1FAIpQLSeu-HXK4EyWAwkkfn5lYWMmY0joMTCzC-Khc1tTVdaEfrzCjw/viewform"
+				>
+					Sign up and request to present your organization here.
+				</a>
+			</p>
+		</Card>
+		<Card>
+			<h1>Delegates</h1>
+			<p>
+				Grade 7 & 8 students in the Peel Region can
+				<a href="https://drive.google.com/file/d/1qawS--JDHKHEyMDq88HRaRkxoovIdGef/view">
+					apply as a delegate.
+				</a>
+				The application is due on May 30th, 11:59 PM
+			</p>
+			<p>
+				<b>
+					Delegates are required to pay a $31 entrance fee, which covers a tshirt, tote bag, and
+					food for the entire day!
+				</b>
+			</p>
+		</Card>
+		<Card>
+			<h1>Partnership</h1>
+			<p>
+				Please contact
+				<a href="mailto:soar.chairs@gmail.com?subject=Partnership with SOAR!">
+					soar.chairs@gmail.com
+				</a>
+				if you are interested in contributing to the success of this conference! You can find our sponsorship
+				package here. You can be an in-kind sponsor or even provide a monetary or goods donation.
+			</p>
+		</Card>
+		<Card>
+			<div class="image">
+				<img
+					src="https://plus.unsplash.com/premium_photo-1673697239984-b089baf7b6e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmFja2dyb3VuZCUyMGltYWdlfGVufDB8fDB8fHww&w=1000&q=80"
+					alt="Background"
+				/>
+			</div>
+		</Card>
+		<Card>
+			<h1>Volunteers</h1>
+			<p>
+				Grade 9, 10, 11 and 12 students across PDSB can
+				<a
+					href="https://drive.google.com/file/d/19faaSDeiJjA4bqjrdVTxRwcdOR2YEwu5/view?usp=sharing"
+				>
+					apply to be a volunteer.
+				</a>
+				If you are in Grade 9, do not worry as there will be opportunities for you in the future! You
+				do not need past experience in SOAR to apply.
+			</p>
+		</Card>
 		<div class="overflow-card">
-			<Card>9</Card>
+			<Card>
+				<div class="image">
+					<img
+						src="https://plus.unsplash.com/premium_photo-1673697239984-b089baf7b6e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmFja2dyb3VuZCUyMGltYWdlfGVufDB8fDB8fHww&w=1000&q=80"
+						alt="Background"
+					/>
+				</div>
+			</Card>
 		</div>
+		<Card>
+			<h1>Further Questions</h1>
+			<p>
+				Applications will be made available in March, and successful applicants will be interviewed.
+				If you have any other questions or concerns, please email us at
+				<a href="mailto:soar.chairs@gmail.com?subject=A question!"> soar.chairs@gmail.com </a>
+				We will be happy to assist you.
+			</p>
+		</Card>
 	</div>
 </div>
